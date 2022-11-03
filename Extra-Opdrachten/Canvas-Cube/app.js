e170ef654c7d386dc27a10b6ca17883f937f14bd
SKoster1210 <9018469@student.zadkine.nl>
2022-11-03 10:58:42 +0100

    Canvas Cube opdracht | Geen tweede cube

--- Extra-Opdrachten/Canvas-Cube/app.js
+++ Extra-Opdrachten/Canvas-Cube/app.js
@@ -3,16 +3,18 @@ let updown = 50;
 
 window.addEventListener("load", function () {
     const canvas = document.querySelector("canvas");
-    //context zorgt ervoor dat je dingen toe kunt voegen aan je canvas
-    // zoals een vierkant oid
     const ctx = canvas.getContext("2d");
-    //Grootte aanpassen
-    //clg canvas en kijk of je de hoogte kan vinden en aanpassen
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
-    //maak drie vierkanten op het scherm met de kleuren blauw, rood en geel
     ctx.fillStyle = "blue";
     ctx.fillRect(side, updown, 200,200);
+
+    // const boxcanvas = document.getElementById("box");
+    // const boxctx = canvas.getContext("2d");
+    // boxcanvas.height = window.innerHeight;
+    // boxcanvas.width = window.innerWidth;
+    // boxctx.fillStyle = "blue";
+    // boxctx.fillRect(100,100, 200,200);
 
 })
 
@@ -22,33 +24,75 @@ function moveCube(e) {
     let keyPressed = e.key
     const canvas = document.querySelector("canvas");
     const ctx = canvas.getContext("2d");
+    let Bounding = canvas.getBoundingClientRect()
     if (keyPressed == "w") {
-        updown = updown - 2 
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.moveTo(0, 0);
-        ctx.fillStyle = "blue";
-        ctx.fillRect(side, updown, 200,200);
-        ctx.stroke();
+        if (updown >= 2) {
+            updown = updown - 2 
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.moveTo(0, 0);
+            ctx.fillStyle = "blue";
+            ctx.fillRect(side, updown, 200,200);
+            ctx.stroke();
+        } else {
+            alert("Top kant heeft de border geraakt.")
+            updown = 150
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.moveTo(0, 0);
+            ctx.fillStyle = "blue";
+            ctx.fillRect(side, updown, 200,200);
+            ctx.stroke();
+        }
     } if (keyPressed == "a") {
-        side = side - 2 
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.moveTo(0, 0);
-        ctx.fillStyle = "blue";
-        ctx.fillRect(side, updown, 200,200);
-        ctx.stroke();
+        if (side >= 2) {
+            side = side - 2 
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.moveTo(0, 0);
+            ctx.fillStyle = "blue";
+            ctx.fillRect(side, updown, 200,200);
+            ctx.stroke();
+        } else {
+            alert("Linker kant heeft de border geraakt.")
+            side = 600
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.moveTo(0, 0);
+            ctx.fillStyle = "blue";
+            ctx.fillRect(side, updown, 200,200);
+            ctx.stroke();
+        }
     } if (keyPressed == "d") {
-        side = side + 2 
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.moveTo(0, 0);
-        ctx.fillStyle = "blue";
-        ctx.fillRect(side, updown, 200,200);
-        ctx.stroke();
+        console.log(side);
+        if (side <= 1334) {
+            side = side + 2 
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.moveTo(0, 0);
+            ctx.fillStyle = "blue";
+            ctx.fillRect(side, updown, 200,200);
+            ctx.stroke();
+        } else {
+            alert("Rechter kant heeft de border geraakt.")
+            side = 600
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.moveTo(0, 0);
+            ctx.fillStyle = "blue";
+            ctx.fillRect(side, updown, 200,200);
+            ctx.stroke();
+        } 
     } if (keyPressed == "s") {
-        updown = updown + 2 
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.moveTo(0, 0);
-        ctx.fillStyle = "blue";
-        ctx.fillRect(side, updown, 200,200);
-        ctx.stroke();
+        if (updown <= 510) {
+            updown = updown + 2 
+             ctx.clearRect(0, 0, canvas.width, canvas.height);
+             ctx.moveTo(0, 0);
+             ctx.fillStyle = "blue";
+             ctx.fillRect(side, updown, 200,200);
+             ctx.stroke();
+        } else {
+            alert("Bottom kant heeft de border geraakt.")
+            updown = 300
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.moveTo(0, 0);
+            ctx.fillStyle = "blue";
+            ctx.fillRect(side, updown, 200,200);
+            ctx.stroke();
+        }
     }
 }
